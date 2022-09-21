a742858b79f91d4621d6b9501cfd8b1e3c2f39a4
pedro-git-projects <pedro.generic.email@gmail.com>
2022-09-20 22:02:36 -0300

    adicionando escolha do exercicio

---
+++ exercicios/utils/escolha.js
@@ -0,0 +1,34 @@
+import prompt from "prompt-sync";
+let reader = prompt();
+
+import { ex1 } from "../ex1.js";
+import { ex2 } from "../ex2.js";
+import { ex3 } from "../ex3.js";
+import { ex4 } from "../ex4.js";
+import { ex5 } from "../ex5.js";
+
+export const escolha = () => {
+	let input = reader("-> Por favor, escolha um exercício digitando um número entre 1 e 5: ") 
+	switch(input) {
+		case "1":
+			console.log(`Exercício 1 escolhido: `)
+			ex1();
+			break;
+		case "2":
+			console.log(`Exercício 2 escolhido: `)
+			ex2();
+			break;
+		case "3":
+			console.log(`Exercício 3 escolhido: `)
+			ex3();
+			break;
+		case "4":
+			console.log(`Exercício 4 escolhido: `)
+			ex4();
+			break;
+		case "5":
+			console.log(`Exercício 5 escolhido: `)
+			ex5();
+			break;
+	}	
+}
--- main.js
+++ main.js
@@ -1,11 +1,3 @@
-import {ex1} from "./exercicios/ex1.js";
-import {ex2} from "./exercicios/ex2.js";
-import {ex3} from "./exercicios/ex3.js";
-import {ex4} from "./exercicios/ex4.js";
-import {ex5} from "./exercicios/ex5.js";
+import { escolha } from "./exercicios/utils/escolha.js"
 
-ex1();
-ex2();
-ex3();
-ex4();
-ex5();
+escolha();
